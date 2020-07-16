96be52588fb2c2b60708f247efcd4808c48a0e8f
Rohith Ramesh <rohith.sharma.1995@gmail.com>
2020-07-16 19:57:25 +0530

    Add files via upload

--- src/main/java/com/test/quarkus/model/Flight.java
+++ src/main/java/com/test/quarkus/model/Flight.java
@@ -1,49 +1,77 @@
 package com.test.quarkus.model;
 
-
 import java.util.Date;
-import javax.persistence.Entity;
+import java.util.UUID;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 @Entity
-public class Flight extends PanacheEntity {
-
-	long flightId;
+public class Flight extends PanacheEntityBase {
+	@Id
+	@GeneratedValue
+	UUID id;
+	
 	String flightBrand;
-	Date flighttime;
-	String fromcity;
-	String tocity;
-	public long getFlightId() {
-		return flightId;
+	Date flightTime;
+	String fromCity;
+	String toCity;
+	int totalSeats;
+	int seatsFilled;
+
+	public UUID getFlightId() {
+		return this.id;
 	}
-	public void setFlightId(long flightId) {
-		this.flightId = flightId;
-	}
+
 	public String getFlightBrand() {
-		return flightBrand;
+		return this.flightBrand;
 	}
+
 	public void setFlightBrand(String flightBrand) {
 		this.flightBrand = flightBrand;
 	}
-	public Date getFlighttime() {
-		return flighttime;
+
+	public Date getFlightTime() {
+		return this.flightTime;
 	}
-	public void setFlighttime(Date flighttime) {
-		this.flighttime = flighttime;
+
+	public void setFlightTime(Date flightTime) {
+		this.flightTime = flightTime;
 	}
-	public String getFromcity() {
-		return fromcity;
+
+	public String getFromCity() {
+		return this.fromCity;
 	}
-	public void setFromcity(String fromcity) {
-		this.fromcity = fromcity;
+
+	public void setFromCity(String fromCity) {
+		this.fromCity = fromCity;
 	}
-	public String getTocity() {
-		return tocity;
+
+	public String getToCity() {
+		return this.toCity;
 	}
-	public void setTocity(String tocity) {
-		this.tocity = tocity;
+
+	public void setToCity(String toCity) {
+		this.toCity = toCity;
 	}
-	
-	
+
+	public int getTotalSeats() {
+		return this.totalSeats;
+	}
+
+	public void setTotalSeats(int totalSeats) {
+		this.totalSeats = totalSeats;
+	}
+
+	public int getSeatsFilled() {
+		return this.seatsFilled;
+	}
+
+	public void setSeatsFilled(int seatsFilled) {
+		this.seatsFilled = seatsFilled;
+	}
+
 }
